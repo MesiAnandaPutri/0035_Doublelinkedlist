b2efea84eddf60687bcf346031a65089bd3c0a7b
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-04 10:21:59 +0700

    case traverse

--- double/double.cpp
+++ double/double.cpp
@@ -221,4 +221,7 @@ int main()
             case '2':
                 deleteNode();
                 break;
-          
+            case '3':
+                traverse();
+                break;
+           
